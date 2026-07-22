e6d4781132d1c3e1c9b0dfa5c4a8969b65a8652e
Mgirijashree <mgirija.shree@gmail.com>
2026-07-22 10:20:37 +0530

    Configure Django for Vercel serverless

---
+++ api/index.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(__file__)
+    )
+)
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "fintrack.settings"
+)
+
+app = get_wsgi_application()
